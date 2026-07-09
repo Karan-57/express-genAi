2cf06d3c7ec8fe3554c16c757cc5634d1a9fbae7
karan-57 <porcheixii@gmail.com>
2026-07-09 21:09:00 +0530

    Initialize project and database configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Server connected to database");
+    }catch(err){
+        console.log("Error connecting to database:\n",err);
+    }
+}
+
+export default connectDB;
